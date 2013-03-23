9e47a0fd8d8dd1d60905b852f2bd4e11a0788f96
andre de oliveira <deoliveirambx@googlemail.com>
2013-03-23 13:08:42 -0300

    adding initial work on makefiles

---
+++ searchengine/tests/run-tests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LIBENGINEDIR}
+for i in test*; do
+	if [ -x ${i} -a x${i} != xrun-tests.sh ]; then
+	    case "$1" in
+		-convert )
+		    eval ./${i} -xml | ./qtest-to-hudson.py "${i}" \
+				> reports/${i}.xml
+		    ;;
+		-xml )
+		    eval ./${i} -xml > reports/${i}.xml
+		    ;;
+		* )
+		    eval ./${i}
+		    ;;
+	    esac
+	fi
+done
