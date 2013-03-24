0ca8264a738e23759bf639478bfecb95214aae62
andre de oliveira <deoliveirambx@googlemail.com>
2013-03-24 18:21:58 -0300

    initial work on index.h

---
+++ searchengine/index.h
@@ -0,0 +1,16 @@
+#ifndef INDEX_H
+#define INDEX_H
+
+#include <QString>
+#include <QStringList>
+#include <QMap>
+
+#if 1
+class Index : public QMap<QString, QStringList>
+{
+};
+#endif
+
+//typedef QMap<QString, QStringList> Graph;
+
+#endif // !INDEX_H
