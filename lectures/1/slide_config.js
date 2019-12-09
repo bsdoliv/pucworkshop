5598f8d67f92c49d27da159f3e2fa37b8ad1da83
andre de oliveira <deoliveirambx@googlemail.com>
2019-12-09 13:30:18 -0300

    Add lectures folder, and first session content

---
+++ lectures/1/slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Introdução à Qt<br>Construindo um Search Engine',
+    subtitle: 'Módulo 1: Boas vindas e Introdução à Qt',
+    eventTitle: '2013.04 Computação / PUC-SP',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/trolltech.jpg',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+    /* gplus: 'Not Yet',
+    twitter: '@QtLabMeetup',
+	  */
+
+  // Author information
+  presenters: [{
+    name: 'André de Oliveira',
+    company: 'chocolatelabz.com',
+    twitter: '@bsdoliv',
+    www: 'http://www.chocolatelabz.com/~bsdoliv/',
+    github: 'https://github.com/bsdoliv/pucworkshop'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
